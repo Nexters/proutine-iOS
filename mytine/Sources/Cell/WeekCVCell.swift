7ed5ea63b49f76c855af2dc0d7b6d21c96231055
ttub-nii <ostarangelo@naver.com>
2020-07-20 17:28:40 +0900

    add: cell model

---
+++ mytine/Sources/Cell/WeekCVCell.swift
@@ -0,0 +1,13 @@
+//
+//  WeekCVCell.swift
+//  mytine
+//
+//  Created by 황수빈 on 2020/07/20.
+//  Copyright © 2020 황수빈. All rights reserved.
+//
+
+import UIKit
+
+class WeekCVCell: UICollectionViewCell {
+    
+}
