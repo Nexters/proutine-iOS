0cb37996bc5f2d8ee998353e16ce7839fe4b6197
ttub-nii <ostarangelo@naver.com>
2020-07-20 17:29:50 +0900

    add: home popup view

---
+++ mytine/Sources/HomePopVC.swift
@@ -0,0 +1,26 @@
+//
+//  HomePopVC.swift
+//  mytine
+//
+//  Created by 황수빈 on 2020/07/20.
+//  Copyright © 2020 황수빈. All rights reserved.
+//
+
+import UIKit
+
+class HomePopVC: UIViewController {
+
+    @IBOutlet var backView: UIView!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var editBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        backView.viewRounded(cornerRadius: 10)
+        imageView.viewRounded(cornerRadius: 10)
+        editBtn.viewRounded(cornerRadius: 14)
+    }
+    
+    @IBAction func clickClose(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
