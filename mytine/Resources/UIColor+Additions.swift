01240385c06085b3d8a71f3a939d5b7b64f3537b
ttub-nii <ostarangelo@naver.com>
2020-07-22 03:10:39 +0900

    update: uicolor extensions

--- mytine/Resources/UIColor+Additions.swift
+++ mytine/Resources/UIColor+Additions.swift
@@ -12,4 +12,8 @@ extension UIColor {
     @nonobjc class var mainGreen: UIColor {
         return UIColor(red: 0 / 255.0, green: 226 / 255.0, blue: 119 / 255.0, alpha: 1.0)
     }
+    
+    @nonobjc class var doneColor: UIColor {
+        return UIColor(red: 166 / 255.0, green: 166 / 255.0, blue: 166 / 255.0, alpha: 1.0)
+    }
 }
