a97269bcf7daa8344de8c86e7060abe2b16ad52d
namsoo5 <are34@naver.com>
2020-08-22 15:40:37 +0900

    UPDATE: splashVC 생성

---
+++ mytine/Sources/Screen/Splash/Views/SplashViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplashViewController.swift
+//  mytine
+//
+//  Created by 남수김 on 2020/08/22.
+//  Copyright © 2020 황수빈. All rights reserved.
+//
+
+import UIKit
+import Lottie
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
